705b0f8e927405a9c955537409ed3414105a4ec7
Tayfun <tayfun.sevimli@outlook.com>
2022-04-04 10:29:49 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(magnet)
+})
+let magnet = 0
+magnet = 0
+radio.setGroup(1)
+basic.forever(function () {
+    magnet = input.magneticForce(Dimension.Strength)
+    if (magnet < 400) {
+        radio.sendString("open")
+    } else {
+        radio.sendString("closed")
+    }
+})
